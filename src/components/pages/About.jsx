da81a8a8b241fe539df8a4008f34616ea6251f22
Wumi Afo <117525566+babycodeher@users.noreply.github.com>
2023-06-06 23:00:41 +0100

    updated contact page

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -21,35 +21,31 @@ function About() {
                         During the course of the bootcamp, I immersed myself in various web development technologies including HTML, CSS, JavaScript, JQuery and frameworks like React.js. I am currently sharpening my knowledge of CSS framworks such as Tailwind and UIKit and improving on my experience in building responsive and user-friendly websites and applications.
                     </p>
                 </div>
-                <div className='flex flex-col justify-center items-center text-center pt-2'>
+                
+                <div className='flex flex-col justify-center items-center text-center pt-16 p-24'>
                     <h1 className="skills text-3xl font-medium text-center md:4xl">
                         Skills
                     </h1>
                     <p className="my-4">These are the technologies I've been learning and working with</p>
-                    <div className="container flex flex-col-reverse justify-evenly gap-4 px-6 py-10 mx-auto space-y-12 md:flex-row md:space-y-0">
+                    <div className="container flex flex-col justify-evenly gap-4 px-6 py-10 mx-auto space-y-12 md:flex-row md:space-y-0">
                         <div className="shadow-md shadow-[#472D30] hover:scale-110 duration-500">
-                            <FontAwesomeIcon icon={faJsSquare} size="4x" className="text-red-500" />
-                            <p className='my-4'>JavaScript</p>
+                            <FontAwesomeIcon icon={faJsSquare} size="4x" className="text-black-500" />
                         </div>
                         <div className="shadow-md shadow-[#472D30] hover:scale-110 duration-500">
-                            <FontAwesomeIcon icon={faHtml5} size="4x" className="text-yellow-500" />
-                            <p className='my-4'>HTML</p>
+                            <FontAwesomeIcon icon={faHtml5} size="4x" className="text-black-500" />
                         </div>
                         <div className="shadow-md shadow-[#472D30] hover:scale-110 duration-500">
-                            <FontAwesomeIcon icon={faCss3Alt} size="4x" className="text-blue-500" />
-                            <p className='my-4'>CSS</p>
+                            <FontAwesomeIcon icon={faCss3Alt} size="4x" className="text-black-500" />
                         </div>
                         <div className="shadow-md shadow-[#472D30] hover:scale-110 duration-500">
-                            <FontAwesomeIcon icon={faReact} size="4x" className="text-purple-500" />
-                            <p className='my-4'>React</p>
+                            <FontAwesomeIcon icon={faReact} size="4x" className="text-black-500" />
                         </div>
                         <div className="shadow-md shadow-[#472D30] hover:scale-110 duration-500">
                             <FontAwesomeIcon icon={faBootstrap} size="4x" className="text-black-500" />
-                            <p className='my-4'>Bootstrap</p>
                         </div>
                         <div className="shadow-md shadow-[#472D30] hover:scale-110 duration-500">
-                            <FontAwesomeIcon icon={faNode} size="4x" className="text-green-500" />
-                            <p className='my-4'>Node.js</p>
+                            <FontAwesomeIcon icon={faNode} size="4x" className="text-black-500" />
+
                         </div>
                     </div>
                 </div>
--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -19,7 +19,7 @@ function ContactForm() {
     };
 
     return (
-        <div className="container flex flex-1 flex-col md:flex-row items-center px-6 mx-auto mt-10 space-y-0 md:space-y-0">
+        <div className="container flex flex-1 justify-center md:flex-row items-center px-6 mx-auto mt-10 space-y-0 md:space-y-0">
             {/* left item */}
             <div className="flex flex-col justify-center items-center space-y-12 sm:w-full md:w-1/2">
                 <h1 className="max-w-md text-4xl font-semibold text-center md:text-5xl md:text-left">
@@ -91,10 +91,10 @@ function ContactForm() {
                 </form>
             </div>
             {/* right item */}
-            <div className="flex flex-col justify-center items-center space-y-12 md:w-1/2">Contact details
+            {/* <div className="flex flex-col justify-center items-center space-y-12 md:w-1/2">Contact details
                 <div class="box-content h-32 w-32 bg-gray-400">
                 </div>
-            </div>
+            </div> */}
         </div>
     );
 }
